a40ada4b64b7f9b8e0322cee439bacaae5dc1c0e
ParthivPatel-BTC <parthiv.patel@botreeconsulting.com>
2015-06-01 10:57:58 +0530

    Added lat long logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :get_cookie_values
+
+  def get_cookie_values
+    @latitude = cookies[:latitude]
+    @longitude = cookies[:longitude]
+  end
 end
